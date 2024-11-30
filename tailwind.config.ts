6c2e479b4f1226e3da68deb7643fd9009145d6cd
zaynom <ouaziz0626@gmail.com>
2024-11-30 18:38:27 +0100

    Add some configs

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -11,6 +11,17 @@ export default {
       colors: {
         background: "var(--background)",
         foreground: "var(--foreground)",
+        primary: "#9333ea",
+      },
+    },
+    container: {
+      center: true,
+      padding: {
+        DEFAULT: "1rem",
+        sm: "2rem",
+        lg: "4rem",
+        xl: "5rem",
+        "2xl": "6rem",
       },
     },
   },
