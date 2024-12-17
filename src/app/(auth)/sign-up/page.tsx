406653af3f0c5cb6d613f027af6579d0252dfda4
zaynom <ouaziz0626@gmail.com>
2024-12-18 00:50:21 +0100

    Rename sign up route folder

---
+++ src/app/(auth)/sign-up/page.tsx
@@ -0,0 +1,43 @@
+import { Separator } from "@/components/ui/separator";
+import { getLoggedInUser, signUpWithGoogle } from "@/repositories/authRepo";
+import Link from "next/link";
+import { redirect } from "next/navigation";
+import { FcGoogle } from "react-icons/fc";
+
+export default async function SignUpPage() {
+  const user = await getLoggedInUser();
+  if (user) redirect("/dashboard");
+
+  return (
+    <>
+      <div className="grid place-items-center h-screen">
+        <div className="w-[400px] h-72 shadow-lg rounded-lg bg-white flex flex-col items-center p-10 gap-7">
+          <div>
+            <h2 className="text-lg font-bold text-center">
+              Create your account
+            </h2>
+            <span className="text-sm text-gray-500">
+              Welcome! Please fill in the details to get started.
+            </span>
+          </div>
+          <form action={signUpWithGoogle}>
+            <button
+              type="submit"
+              className="flex items-center text-lg text-gray-700 gap-2 border py-3 px-6 rounded-lg shadow-md"
+            >
+              <FcGoogle size={24} />
+              Continue with Google
+            </button>
+          </form>
+          <Separator />
+          <span className="text-sm text-gray-500">
+            Already have an account?{" "}
+            <Link href="/sign-in" className="hover:underline text-black">
+              Sign in
+            </Link>
+          </span>
+        </div>
+      </div>
+    </>
+  );
+}
