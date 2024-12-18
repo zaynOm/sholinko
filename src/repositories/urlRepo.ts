e61ad5c9253e5cda8964c2de489939c0343274d0
zaynom <ouaziz0626@gmail.com>
2024-12-18 01:02:53 +0100

    Create user links counter function

--- src/repositories/urlRepo.ts
+++ src/repositories/urlRepo.ts
@@ -3,6 +3,18 @@ import { createAdminClient } from "@/lib/server/appwrite";
 import { ID, Query } from "node-appwrite";
 import { randomBytes } from "crypto";
 
+async function getLinksCount(userId: string) {
+  const { database } = await createAdminClient();
+
+  const result = await database.listDocuments(
+    env.DATABASE_ID,
+    env.COLLECTION_LINKS_ID,
+    [Query.equal("userId", userId)],
+  );
+
+  return result.documents.length;
+}
+
 async function generateSlug() {
   const slug = randomBytes(5).toString("base64").slice(0, 5);
   if (!isSlugAvailibale(slug)) {
