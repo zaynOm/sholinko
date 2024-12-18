35484e56eb8686b2d58a8dbb150c89f4f29ab22c
zaynom <ouaziz0626@gmail.com>
2024-12-18 01:05:07 +0100

    Add new link creation function

--- src/repositories/urlRepo.ts
+++ src/repositories/urlRepo.ts
@@ -1,7 +1,33 @@
 import { env } from "@/env";
 import { createAdminClient } from "@/lib/server/appwrite";
 import { ID, Query } from "node-appwrite";
+import { getLoggedInUser } from "./authRepo";
+import { formType } from "@/app/dashboard/links/new/page";
 import { randomBytes } from "crypto";
+
+export async function createNewLink(data: formType) {
+  const { database } = await createAdminClient();
+  const user = await getLoggedInUser();
+
+  if (!user) return;
+
+  const linksCount = await getLinksCount(user?.$id);
+  const slug = await generateSlug();
+
+  const newLink = {
+    userId: user?.$id,
+    originalUrl: data.originalUrl,
+    title: data.title || `Link ${linksCount + 1}`,
+    shortUrl: slug,
+  };
+
+  await database.createDocument(
+    env.DATABASE_ID,
+    env.COLLECTION_LINKS_ID,
+    ID.unique(),
+    newLink,
+  );
+}
 
 async function getLinksCount(userId: string) {
   const { database } = await createAdminClient();
