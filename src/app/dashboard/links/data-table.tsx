9f2368053d26e0cfcc7264012c47cffdc40e6d00
zaynom <ouaziz0626@gmail.com>
2025-01-14 18:07:00 +0100

    Update original link table cell to truncate and take less width

--- src/app/dashboard/links/data-table.tsx
+++ src/app/dashboard/links/data-table.tsx
@@ -57,7 +57,11 @@ export function DataTable<TData, TValue>({
             table.getRowModel().rows.map((row) => (
               <TableRow key={row.id}>
                 {row.getVisibleCells().map((cell) => (
-                  <TableCell key={cell.id}>
+                  <TableCell
+                    key={cell.id}
+                    style={{ maxWidth: cell.column.columnDef.size }}
+                    className="truncate"
+                  >
                     {flexRender(cell.column.columnDef.cell, cell.getContext())}
                   </TableCell>
                 ))}
